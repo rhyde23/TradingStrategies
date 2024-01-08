dd95fdd76114b29b9aca174d1ef19f1254706ed2
rhyde23 <reginaldjhyde@gmail.com>
2024-01-08 17:21:56 -0600

    Updated "calculateRSI" to account for the "smoothing" effect, also added some notes.

--- calculateRSI.py
+++ calculateRSI.py
@@ -1,8 +1,15 @@
 #This script calculates the Relative Strength Index (RSI) based on a given stock ticker and time period in days
+
+#Import script that accesses Python library yfinance
 import getStockInfo
-def calculate_rsi(ticker:str, period_in_days:int) :
-    hist_data = getStockInfo.get_historical_data(ticker, str(period_in_days+1)+"D")
-    print(hist_data["Close"])
+
+#Function to calculate RSI
+#Arguments are: desired stock ticker, the RSI period, and the number of historical days to achieve "smoothing effect"
+def calculate_rsi(ticker:str, period_in_days:int, historical_context:int) :
+    #Access stock's historical data from yahoo finance
+    hist_data = getStockInfo.get_historical_data(ticker, str(historical_context)+"D")
+
+    #This first loop calculates initial RSI value in the past before smoothing
     gain_sum, loss_sum = 0, 0
     for i in range(1, period_in_days+1) :
         difference = hist_data.iloc[i]["Close"]-hist_data.iloc[i-1]["Close"]
@@ -10,8 +17,20 @@ def calculate_rsi(ticker:str, period_in_days:int) :
             gain_sum += difference
         else :
             loss_sum += -difference
-    avg_gain, avg_loss = gain_sum/period_in_days, loss_sum/period_in_days
-    rs = avg_gain/avg_loss
+    avg_gain, avg_loss = gain_sum/period_in_days, loss_sum/period_in_days #Non-smoothed RSI in the distant past
+
+    #This loop smooths the RSI until we reach the current date
+    for x in range(period_in_days+1, historical_context) :
+        difference = hist_data.iloc[x]["Close"]-hist_data.iloc[x-1]["Close"]
+        if difference >= 0 :
+            avg_gain = ((avg_gain*13)+difference)/14
+            avg_loss = (avg_loss*13)/14
+        else :
+            avg_gain = (avg_gain*13)/14
+            avg_loss = ((avg_loss*13)-difference)/14
+    #Finally, we calculate the relative strength value and then return the RSI in terms of a value 0-100
+    rs = (avg_gain/avg_loss)
     return 100-(100/(1+rs))
 
-print(calculate_rsi("MSFT", 14))
+print(calculate_rsi("NVDA", 14, 1000))
+
