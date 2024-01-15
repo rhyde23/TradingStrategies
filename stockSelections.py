c95164e9b808194382333bde40028a2222181775
rhyde23 <reginaldjhyde@gmail.com>
2024-01-14 21:24:02 -0600

    Added some notation and added a new selection strategy.

--- stockSelections.py
+++ stockSelections.py
@@ -1,32 +1,41 @@
-#This script is a temporary strategy of picking which stocks to apply automated technical analysis on
-#Selects all the stocks from the top 100 most active stocks by volume and filters out stocks with low relative volume
-#Scrapes information from tradingview.com
+#This script contains all of the possible stock selection strategies
 
 #Import Web Scraping libraries
 import requests 
 from bs4 import BeautifulSoup 
 
-#Function to extract the list of 100 most traded stocks from tradingview.com and filters out stocks that don't meet minimum relative volume 
-def selection1(minimum_relative_volume:int) :
-    #Processes url request
-    URL = "https://www.tradingview.com/markets/stocks-usa/market-movers-active/" 
-    r = requests.get(URL)
+#Selection strategy that extracts the list of 100 most traded stocks from tradingview.com and filters out stocks that don't meet the minimum relative volume of 1.5
+def selection1() :
     
+    #Processes url request for tradingview.com of the most active stocks of the day
+    r = requests.get("https://www.tradingview.com/markets/stocks-usa/market-movers-active/")
+
+    #Apply BeautifulSoup() to the website request
     soup = BeautifulSoup(r.content, 'html.parser')
+
+    #Empty list that will be populated with stock tickers that meet the relative volume criteria
     accepted = []
+
+    #Loop that iterates through each stock in the top 100 most traded stocks of the day from tradingview.com
     for stock in soup.find_all("tr", {"class":"row-RdUXZpkv listRow"}) :
-        #Get stock ticker
+        
+        #Get the ticker of the current stock
         ticker = stock.attrs['data-rowkey'].split(":")[1]
+        
         #Get its relative volume and accept stock if its relative volume is greater or equal to the minimum relative volume input
         try :
             relative_volume = float(stock.find_all("td", {"class":"cell-RLhfr_y4 right-RLhfr_y4"})[3].contents[0])
-            if relative_volume >= minimum_relative_volume :
+            if relative_volume >= 1.5 :
                 accepted.append(ticker)
         except :
             pass
+        
     #Return accepted list of stock tickers
     return accepted
 
+#Selection strategy that simply returns the "Big 7" tech stock of the modern era
+def selection2() :
+    return ["AAPL", "AMZN", "MSFT", "NVDA", "META", "TSLA", "GOOG"]
 
-#This script is just a potential strategy for locating which stocks to apply technical analysis, not the holy grail
-print(selection1(2))
+
+print(selection2())
