e50cfaca1f25551b66aae21aeb3b90373770a32c
rhyde23 <reginaldjhyde@gmail.com>
2024-01-23 11:29:11 -0600

    Finished Yahoo Finance selenium scraper

--- script.py
+++ script.py
@@ -29,31 +29,7 @@ if not check_up_to_date("yfinance") :
 #Selection Strategies
 
 def selection1() :
+    pass
     
-    #Processes url request for tradingview.com of the most active stocks of the day
-    #r = requests.get("https://www.investing.com/portfolio/?portfolioID=YGAyYTVlYTVkMWFlM2YwOg%3D%3D")
-    #r = requests.get("https://www.googletagservices.com/activeview/js/current/ufs_web_display.js?cache=r20110914")
-
-    #Apply BeautifulSoup() to the website request
-    #soup = BeautifulSoup(r.content, 'html.parser')
-    #print(soup)
-    #print("NVDA" in soup)
-    
-    r = session.get('https://www.googletagservices.com/activeview/js/current/ufs_web_display.js?cache=r20110914')
-
-    r.html.render()
-    #print(soup.find_all("td", {"data-column-name":"symbol"}))
-    
-    #for element in soup.find_all("tr") :#{"class":"datatable-v2_cell__IwP1U dynamic-table-v2_col-other__zNU4A text-right rtl:text-right"}) :
-        #print(element)
-        #print(finstreamer)
-        #t, price = finstreamer = finstreamer.attrs['data-symbol'], float(finstreamer.attrs['value'])
-        #print(t, price)
-        #class="datatable-v2_cell__IwP1U dynamic-table-v2_col-other__zNU4A text-right rtl:text-right"
-        #print(finstreamer.attrs["class"])]
-        #print(finstreamer.find_all("div", {"class":"flex justify-end"})[0])
-        #print(finstreamer)
-        #prin
-    #print(soup)
 
 selection1()
--- test_html_session.py
+++ test_html_session.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 
@@ -17,7 +16,7 @@ except :
     sign_in = driver.find_element(By.XPATH, "//div[@id='login-container']/a[1]")
     sign_in.click()
 
-time.sleep(5)
+time.sleep(3)
 driver.fullscreen_window()
 
 username = driver.find_element(By.ID, "login-username")
@@ -26,7 +25,7 @@ username.send_keys("reginaldjhyde@gmail.com")
 next_button = driver.find_element(By.NAME, "signin")
 next_button.click()
 
-time.sleep(5)
+time.sleep(3)
 driver.fullscreen_window()
 
 password = driver.find_element(By.ID, "login-passwd")
@@ -35,13 +34,19 @@ password.send_keys("***REMOVED***")
 next_button2 = driver.find_element(By.NAME, "verifyPassword")
 next_button2.click()
 
-time.sleep(5)
+time.sleep(3)
 
 driver.get("https://finance.yahoo.com/portfolio/p_0/view/v1")
 driver.fullscreen_window()
 
-for d in driver.find_elements(By.TAG_NAME, "fin-streamer") :
-    print(d.text)
+
+while True :
+    tickers = [element.text for element in driver.find_elements(By.XPATH, "//td/a[@data-test='quoteLink']")]
+    prices = [float(element.text) for element in driver.find_elements(By.XPATH, "//td/fin-streamer[@data-field='regularMarketPrice']")]
+
+    for pair in list(zip(tickers, prices)) :
+        print(pair)
+    print()
 
 
-quit()
+
